fbfb9a51d6df3817b8bd37ede46c1a4c944a27e0
Dave Torrente <Dave.Torrente@antser.com>
2021-11-03 09:53:24 +0000

    initial commit homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import data from './data'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">Sample App</a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div>
+          <div className="row center">
+            {
+              data.products.map(product => (
+                <div className="card" key={product.id}>
+                  <a href={`/product/${product.id}`}>
+                    <img className="medium" src={product.image} alt={product.productName} />
+                  </a>
+                  <div className="card-body">
+                  <a href={`/product/${product.id}`}>
+                      <h2>{product.productName}</h2>
+                    </a>
+                    <div className="rating">
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                      <span> <i className="fa fa-star"></i> </span>
+                    </div>
+                    <div className="price">${product.price}</div>
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,76 @@
+const data = {
+    products: [
+        {
+            id: 1,
+            productName: "Product 1",
+            category: "Shirts",
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Sample descrition"
+
+        },
+        {
+            id: 2,
+            productName: "Product 2",
+            category: "Shoes",
+            image: '/images/p2.jpg',
+            price: 100,
+            brand: "Nike",
+            rating: 4.0,
+            numReviews: 10,
+            description: "Sample descrition"
+
+        },
+        {
+            id: 3,
+            productName: "Product 3",
+            category: "Shirts",
+            image: '/images/p3.jpg',
+            price: 220,
+            brand: "Lacoste",
+            rating: 4.5,
+            numReviews: 17,
+            description: "Sample descrition"
+
+        },
+        {
+            id: 4,
+            productName: "Product 4",
+            category: "Pants",
+            image: '/images/p4.jpg',
+            price: 78,
+            brand: "Nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Sample descrition"
+
+        },
+        {
+            id: 5,
+            productName: "Product 5",
+            category: "Pants",
+            image: '/images/p5.jpg',
+            price: 120,
+            brand: "Puma",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Sample descrition"
+
+        },
+        {
+            id: 6,
+            productName: "Product 6",
+            category: "Pants",
+            image: '/images/p6.jpg',
+            price: 139,
+            brand: "Adidas",
+            rating: 4.5,
+            numReviews: 15,
+            description: "Sample descrition"
+        },
+    ]
+}
+export default data;
